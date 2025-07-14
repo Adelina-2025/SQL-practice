e0b02acdb3d240e694547668d7c6592a061728ab
Adelina-2025 <choyu7979@gmail.com>
2025-07-13 21:10:34 -0700

    exercise13_1
    Add: Insert new movie record into movies table

---
+++ exercise13_1.sql
@@ -0,0 +1,3 @@
+-- Exercise13_1
+-- Add the studio's new production, Toy Story 4 to the list of movies (you can use any director)
+INSERT INTO movies VALUES (4, "Toy Story 4", "El Directore", 2015, 90);
